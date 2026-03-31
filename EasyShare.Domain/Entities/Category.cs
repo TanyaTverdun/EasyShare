27627dcc7cbc86846027e9f769371547f7b2ad82
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 00:11:47 +0300

    Add Category entity

---
+++ EasyShare.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyShare.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<ItemType> ItemTypes { get; set; } = new List<ItemType>();
+}
