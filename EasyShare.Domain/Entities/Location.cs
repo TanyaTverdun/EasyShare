81674e627d4a39c9915e1b7d658d318634f8c004
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 00:03:29 +0300

    Add Location entity

---
+++ EasyShare.Domain/Entities/Location.cs
@@ -0,0 +1,13 @@
+﻿namespace EasyShare.Domain.Entities;
+
+public class Location
+{
+    public int Id { get; set; }
+    public string City { get; set; } = string.Empty;
+    public string Street { get; set; } = string.Empty;
+    public int Building { get; set; }
+
+    public ICollection<User> Users { get; set; } = new List<User>();
+    public ICollection<Company> Companies { get; set; } = new List<Company>();
+    public ICollection<Item> Items { get; set; } = new List<Item>();
+}
