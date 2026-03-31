7273f7637b32908113fb5f79ed8037bdd09f97db
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 00:05:17 +0300

    Add User entity

---
+++ EasyShare.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace EasyShare.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public bool IsAdmin { get; set; }
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public int LocationId { get; set; }
+    public Location Location { get; set; } = null!;
+
+    public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+}
