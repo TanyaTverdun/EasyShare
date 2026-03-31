0fd5982cab8dfa5d0d5cbaf59657a4f4a5d43918
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 00:59:26 +0300

    Add LocationConfiguration

---
+++ EasyShare.Infrastructure/Persistence/Configurations/LocationConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using EasyShare.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyShare.Infrastructure.Persistence.Configurations
+{
+    public class LocationConfiguration : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder
+                .ToTable("locations");
+
+            builder
+                .HasKey(l => l.Id);
+
+            builder
+                .Property(l => l.Id)
+                .HasColumnName("location_id");
+
+            builder
+                .Property(l => l.City)
+                .HasColumnName("city")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder
+                .Property(l => l.Street)
+                .HasColumnName("street")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder
+                .Property(l => l.Building)
+                .HasColumnName("building")
+                .IsRequired();
+        }
+    }
+}
