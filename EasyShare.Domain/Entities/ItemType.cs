73cc72adea9905f0f345d441a37cd0f33e462e0e
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 00:12:05 +0300

    Add ItemType entity

---
+++ EasyShare.Domain/Entities/ItemType.cs
@@ -0,0 +1,13 @@
+﻿namespace EasyShare.Domain.Entities;
+
+public class ItemType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+
+    public ICollection<Attribute> Attributes { get; set; } = new List<Attribute>();
+    public ICollection<Item> Items { get; set; } = new List<Item>();
+}
