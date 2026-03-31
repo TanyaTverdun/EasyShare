c79eb4f4e7a416bda63584011544e5b78863399c
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 00:22:39 +0300

    Add Booking entity

---
+++ EasyShare.Domain/Entities/Booking.cs
@@ -0,0 +1,22 @@
+﻿using EasyShare.Domain.Enums;
+
+namespace EasyShare.Domain.Entities;
+
+public class Booking
+{
+    public int Id { get; set; }
+    public short RentedQuantity { get; set; }
+    public DateTimeOffset StartDatetime { get; set; }
+    public DateTimeOffset EndDatetime { get; set; }
+    public decimal TotalPrice { get; set; }
+    public BookingStatus Status { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+
+    public int ItemId { get; set; }
+    public Item Item { get; set; } = null!;
+
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
+
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+}
