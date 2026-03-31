0c4001aecf2f232d6c8053498890fb48c4c5bc11
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 01:06:29 +0300

    Add UserConfiguration

---
+++ EasyShare.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,72 @@
+﻿using EasyShare.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyShare.Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .ToTable("users");
+
+            builder
+                .HasKey(u => u.Id);
+
+            builder
+                .Property(u => u.Id)
+                .HasColumnName("user_id");
+
+            builder
+                .Property(u => u.FirstName)
+                .HasColumnName("first_name")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder
+                .Property(u => u.LastName)
+                .HasColumnName("last_name")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder
+                .Property(u => u.Email)
+                .HasColumnName("email")
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder
+                .Property(u => u.Phone)
+                .HasColumnName("phone")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder
+                .Property(u => u.IsAdmin)
+                .HasColumnName("is_admin")
+                .IsRequired();
+
+            builder
+                .Property(u => u.PasswordHash)
+                .HasColumnName("password_hash")
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder
+                .Property(u => u.LocationId)
+                .HasColumnName("location_id");
+
+            builder
+                .HasOne(u => u.Location)
+                .WithMany(l => l.Users)
+                .HasForeignKey(u => u.LocationId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
