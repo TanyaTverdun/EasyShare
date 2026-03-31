750a72d20781cb61d7cbd29d33e94caa8b8cc642
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 00:06:45 +0300

    Add Company entity

---
+++ EasyShare.Domain/Entities/Company.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyShare.Domain.Entities;
+
+public class Company
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public int LocationId { get; set; }
+    public Location Location { get; set; } = null!;
+
+    public ICollection<Item> Items { get; set; } = new List<Item>();
+}
