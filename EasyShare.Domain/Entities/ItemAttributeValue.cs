d94206cf0a8c8180d54a355fe491a9c7c8c47ffc
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 00:20:43 +0300

    Add ItemAttributeValue

---
+++ EasyShare.Domain/Entities/ItemAttributeValue.cs
@@ -0,0 +1,12 @@
+﻿namespace EasyShare.Domain.Entities;
+
+public class ItemAttributeValue
+{
+    public int ItemId { get; set; }
+    public Item Item { get; set; } = null!;
+
+    public int AttributeId { get; set; }
+    public Attribute Attribute { get; set; } = null!;
+
+    public string Value { get; set; } = string.Empty;
+}
