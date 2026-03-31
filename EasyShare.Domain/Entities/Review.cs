543e24579188333f3f7bc434cae52c112f736ec1
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-01 00:23:57 +0300

    Add Review entity

---
+++ EasyShare.Domain/Entities/Review.cs
@@ -0,0 +1,13 @@
+﻿namespace EasyShare.Domain.Entities;
+
+public class Review
+{
+    public int Id { get; set; }
+    public int Rating { get; set; }
+    public string? Comment { get; set; }
+    public bool IsOwner { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+
+    public int BookingId { get; set; }
+    public Booking Booking { get; set; } = null!;
+}
