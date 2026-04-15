038e8402f57e62e0bdf6a2df4fcf36efcf488995
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-15 14:21:18 +0300

    Add dependency injection configuration for infrastructure

---
+++ EasyShare.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using EasyShare.Application.Common.Interfaces;
+using EasyShare.Application.Common.Interfaces.Services;
+using EasyShare.Infrastructure.Persistence;
+using EasyShare.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyShare.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(
+                    typeof(ApplicationDbContext).Assembly.FullName)));
+
+        services
+            .AddScoped<IApplicationDbContext>(provider =>
+                provider.GetRequiredService<ApplicationDbContext>());
+
+        return services;
+    }
+}
