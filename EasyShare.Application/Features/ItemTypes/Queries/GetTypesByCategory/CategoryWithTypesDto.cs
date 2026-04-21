3ee8c9332cb0d19f7aafccbf80923efc6c9bea98
Tanya Tverdun <tanyatverdun@gmail.com>
2026-04-21 17:09:24 +0300

    Delete unnecessary usings

--- EasyShare.Application/Features/ItemTypes/Queries/GetTypesByCategory/CategoryWithTypesDto.cs
+++ EasyShare.Application/Features/ItemTypes/Queries/GetTypesByCategory/CategoryWithTypesDto.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace EasyShare.Application.Features.ItemTypes.Queries.GetTypesByCategory;
 
-namespace EasyShare.Application.Features.ItemTypes.Queries.GetTypesByCategory
+public record CategoryWithTypesDto
 {
-    public record CategoryWithTypesDto
-    {
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public IEnumerable<ItemTypeDto> Types { get; set; }
-    }
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public IEnumerable<ItemTypeDto> Types { get; set; }
 }
--- EasyShare.Application/Features/ItemTypes/Queries/GetTypesByCategory/ItemTypeDto.cs
+++ EasyShare.Application/Features/ItemTypes/Queries/GetTypesByCategory/ItemTypeDto.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace EasyShare.Application.Features.ItemTypes.Queries.GetTypesByCategory;
 
-namespace EasyShare.Application.Features.ItemTypes.Queries.GetTypesByCategory
+public record ItemTypeDto
 {
-    public record ItemTypeDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
 }
