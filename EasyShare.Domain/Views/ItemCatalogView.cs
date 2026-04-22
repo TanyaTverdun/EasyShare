420ca5ad488b56b53026df6f6855b1bed487bbcb
Tanya Tverdun <tanyatverdun@gmail.com>
2026-04-22 14:25:27 +0300

    Split location into city, street, and building in ItemCatalogView

--- EasyShare.Domain/Views/ItemCatalogView.cs
+++ EasyShare.Domain/Views/ItemCatalogView.cs
@@ -10,7 +10,9 @@ public record ItemCatalogView
     public int TypeId { get; set; }
     public string TypeName { get; init; }
     public string CompanyName { get; init; }
-    public string LocationAddress { get; init; }
+    public string City { get; set; }
+    public string Street { get; set; }
+    public int Building { get; set; }
     public double AverageRating { get; init; }
     public int ReviewsCount { get; init; }
 }
