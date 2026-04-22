1fbb4add2125da1620cca2ff46ea1adb6bd6092b
Tanya Tverdun <tanyatverdun@gmail.com>
2026-04-22 22:08:08 +0300

    add missing column names to ItemCatalogViewConfiguration

--- EasyShare.Infrastructure/Persistence/Configurations/ItemCatalogViewConfiguration.cs
+++ EasyShare.Infrastructure/Persistence/Configurations/ItemCatalogViewConfiguration.cs
@@ -8,8 +8,69 @@ public class ItemCatalogViewConfiguration : IEntityTypeConfiguration<ItemCatalog
 {
     public void Configure(EntityTypeBuilder<ItemCatalogView> builder)
     {
-        builder.ToView("vw_item_catalog");
+        builder
+            .ToView("vw_item_catalog");
 
-        builder.HasNoKey();
+        builder
+            .HasNoKey();
+
+        builder
+            .Property(v => v.ItemId)
+            .HasColumnName("item_id");
+        builder
+            .Property(v => v.StockQuantity)
+            .HasColumnName("stock_quantity");
+
+        builder
+            .Property(v => v.TypeId)
+            .HasColumnName("type_id");
+
+        builder
+            .Property(v => v.CategoryId)
+            .HasColumnName("category_id");
+
+        builder
+            .Property(v => v.Name)
+            .HasColumnName("name");
+
+        builder
+            .Property(v => v.Description)
+            .HasColumnName("description");
+
+        builder
+            .Property(v => v.Price)
+            .HasColumnName("price");
+
+        builder
+            .Property(v => v.ImageUrl)
+            .HasColumnName("image_url");
+
+        builder
+            .Property(v => v.TypeName)
+            .HasColumnName("type_name");
+
+        builder
+            .Property(v => v.CompanyName)
+            .HasColumnName("company_name");
+
+        builder
+            .Property(v => v.City)
+            .HasColumnName("city");
+
+        builder
+            .Property(v => v.Street)
+            .HasColumnName("street");
+
+        builder
+            .Property(v => v.Building)
+            .HasColumnName("building");
+
+        builder
+            .Property(v => v.AverageRating)
+            .HasColumnName("average_rating");
+
+        builder
+            .Property(v => v.ReviewsCount)
+            .HasColumnName("reviews_count");
     }
 }
